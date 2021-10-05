8208adda34fb665f7cc42850d51fb777276dc1d9
Vaithee <vaithi.genghiskhan>
2021-10-06 02:52:29 +0530

    Added BIT logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ import PaymentPortal from './Content/Other pages/Razorpay payment/Payment';
 import Events from './Content/Other pages/Events/Events';
 import PaymentSuccess from './Content/Other pages/Razorpay payment/PaymentSuccess';
 import TandC from './Content/Other pages/T&C/TandC';
+import Temp from './Content/Other pages/Events/Temp'
 //Pages
 
 import "./App.css";
@@ -27,8 +28,8 @@ function App() {
     <Route exact path="/"><Redirect to='/home'/></Route>
     <Route exact path="/home"><div className={!smallerThan1000?"bg_image_home":"bg_image_home_mobile"}><Header/><PageContent/><Foot/></div></Route>
     <Route exact path="/about"><div className={!smallerThan1000?"bg_image_about":"bg_image_about_mobile"}><Header/><About/></div></Route>
-    <Route exact path="/team"><div className={!smallerThan1000?"bg_image_team":"bg_image_team_mobile"}><Header/><Events/></div></Route>
-    <Route exact path="/events"><div className={!smallerThan1000?"bg_image_events":"bg_image_events_mobile"}><Header/><Events/></div></Route>
+    <Route exact path="/team"><div className={!smallerThan1000?"bg_image_team":"bg_image_team_mobile"}><Header/><Temp/></div></Route>
+    <Route exact path="/events"><div className={!smallerThan1000?"bg_image_events":"bg_image_events_mobile"}><Header/><Temp/></div></Route>
     <Route exact path="/contact"><div className={!smallerThan1000?"bg_image_contact":"bg_image_contact_mobile"}><Header/><Contact/></div></Route>
     <Route exact path="/register"><div className={!smallerThan1000?"bg_image_register":"bg_image_register_mobile"}><Header/><Register/></div></Route>
     <Route exact path="/payment"><div className={!smallerThan1000?"bg_image_payment":"bg_image_payment_mobile"}><Header/><PaymentPortal/><FootPayment/></div></Route>
---
+++ src/components/Content/Other pages/Events/Temp.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Component()
+{
+    return(<h1>Coming Soon</h1>)
+}
+export default Component;
