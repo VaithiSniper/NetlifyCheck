9dbf46902027dd72663afd598cbae3cdb9be9543
Vaithee <vaithi.genghiskhan>
2021-10-06 02:54:21 +0530

    Added BIT logo

--- src/components/Content/Other pages/Events/Temp.jsx
+++ src/components/Content/Other pages/Events/Temp.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 function Component()
 {
-    return(<h1>Coming Soon</h1>)
+    return(<h1 style={{textAlign:"center",color:"white",fontSize:"400%"}}>Coming Soon!</h1>)
 }
 export default Component;
