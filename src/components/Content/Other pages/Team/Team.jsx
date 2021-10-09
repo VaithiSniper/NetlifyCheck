3f9f9a8c3ebff83dd522dfa7b68708a08111dd07
Vaithee <vaithi.genghiskhan>
2021-10-09 14:43:03 +0530

    Added trans to team

--- src/components/Content/Other pages/Team/Team.jsx
+++ src/components/Content/Other pages/Team/Team.jsx
@@ -4,11 +4,14 @@ import Card from "@mui/material/Card";
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import './Team.css';
 
 function Component(){
-    return(<Grid container spacing={1} align="center">
-      <Grid container style={{height:"100vh"}} spacing={1} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>Administrative Team</Typography></Grid>
+    return(
+    <Grid container className="page-container">
+    <Grid container spacing={1} align="center" className="scroll-container">
+      <Grid container spacing={1} align="center" className="teamSections">
+      <Grid item lg={12} md={12} sm={12} xs={12} ><Typography variant="h3" style={{color: 'white'}}>Administrative Team</Typography></Grid>
       <Grid item lg={3} md={3} sm={12} xs={12}>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -83,8 +86,8 @@ function Component(){
       </Grid>
       </Grid>
       {/*admin team*/}
-      <Grid container spacing={1} style={{height:"100vh"}} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>Competitive Programming Team</Typography></Grid>
+      <Grid container spacing={1} align="center"  className="teamSections">
+      <Grid item lg={12} md={12} sm={12} xs={12} ><Typography variant="h3" style={{color: 'white'}}>Competitive Programming Team</Typography></Grid>
       <Grid item lg={3} md={3} sm={12} xs={12}>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -159,8 +162,8 @@ function Component(){
       </Grid>
       </Grid>
       {/*cp team*/}
-      <Grid container spacing={1} style={{height:"100vh"}} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>Projects Team</Typography></Grid>
+      <Grid container spacing={1} align="center" className="teamSections" >
+      <Grid item lg={12} md={12} sm={12} xs={12} ><Typography variant="h3" style={{color: 'white'}}>Projects Team</Typography></Grid>
       <Grid item lg={4} md={4} sm={12} xs={12}>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -217,8 +220,8 @@ function Component(){
       </Grid>
       </Grid>
       {/*projects team*/}
-      <Grid container spacing={1} style={{height:"100vh"}} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>Editorial Team</Typography></Grid>
+      <Grid container spacing={1} className="teamSections" align="center">
+      <Grid item lg={12} md={12} sm={12} xs={12} ><Typography variant="h3" style={{color: 'white'}}>Editorial Team</Typography></Grid>
       <Grid item lg={6} md={6} sm={12} xs={12} center>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -257,8 +260,8 @@ function Component(){
       </Grid>
       </Grid>
       {/*editorial team*/}
-      <Grid container spacing={1} style={{height:"100vh"}} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>Event Co-ordinator Team</Typography></Grid>
+      <Grid container spacing={1} align="center" className="teamSections" >
+      <Grid item lg={12} md={12} sm={12} xs={12} ><Typography variant="h3" style={{color: 'white'}}>Event Co-ordinator Team</Typography></Grid>
       <Grid item lg={6} md={6} sm={12} xs={12} center>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -297,8 +300,8 @@ function Component(){
       </Grid>
       </Grid>
       {/*Event Co-ordinator Team*/}
-      <Grid container spacing={1} style={{height:"100vh"}} align="center">
-      <Grid item lg={12} md={12} sm={12} xs={12}><Typography variant="h2" style={{color: 'white'}}>PR & Marketing Team</Typography></Grid>
+      <Grid container spacing={1} align="center" className="teamSections" >
+      <Grid item lg={12} md={12} sm={12} xs={12}  ><Typography variant="h3" style={{color: 'white'}}>PR & Marketing Team</Typography></Grid>
       <Grid item lg={6} md={6} sm={12} xs={12} center>
       <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -338,6 +341,8 @@ function Component(){
       {/*PR & Marketing Team*/}
       </Grid>
       </Grid>
+      </Grid>
+
     )
     }
 export default Component;
