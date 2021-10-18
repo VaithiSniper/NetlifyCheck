242169dfa0fcf8259cb827e2acdcceb38170284b
VaithiSniper <vaithi.genghiskhan@gmail.com>
2021-10-18 18:31:50 +0530

    beautified team page

--- src/components/Content/Other pages/Events/Events.jsx
+++ src/components/Content/Other pages/Events/Events.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 //react
 import Grid from "@mui/material/Grid";
 import Tabs from "./Tabs";
@@ -11,47 +11,48 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 //firestore imports
-
-let arrayOfEvents = [];
+let arrayOfEvents=[];
 //events array, stores all events from firebase
 const firebaseApp = initializeApp({
-    apiKey: 'AIzaSyAZrvXqspVdjVgWWtSmjp7UKpjHotxvJD0',
-    authDomain: 'sdic-22b69.firebaseapp.com',
-    projectId: 'sdic-22b69'
-  });
-  //firebase and firestore setup
-  const db = getFirestore();
-
+  apiKey: 'AIzaSyAZrvXqspVdjVgWWtSmjp7UKpjHotxvJD0',
+  authDomain: 'sdic-22b69.firebaseapp.com',
+  projectId: 'sdic-22b69'
+});
+//firebase and firestore setup
+const db = getFirestore();
 //firebase events retrival
-async function retrival()
-{
 //collection ref
+async function retrival(){
 const querySnapshot = await getDocs(collection(db, "events_test"));
 querySnapshot.forEach((doc) => {
-  // doc.data() is never undefined for query doc snapshots
-  const data = doc.data();
-  arrayOfEvents.push(data);
-});
+// doc.data() is never undefined for query doc snapshots
+const data = doc.data();
+arrayOfEvents.push(data);
+})
+
 }
 function Events() {
-  
-   retrival();
-   console.log(arrayOfEvents[0]);
-   //to populate array
+
+ useEffect(retrival,[]);
+ 
+    if(arrayOfEvents==null)
     return(
-        <Container>
-        <h1 className="events_title">
-        Events
-        </h1>
-        <Grid container spacing={2}>
-      <Grid item lg={12} md={12} sm={12} xs={12}>
-        <Tabs/>
-        <Card
-        />
-      </Grid>
-    </Grid>
-        </Container>
+      <button>Click</button>
     )
+    else{ 
+return(<Container>
+  <h1 className="events_title">
+  Events
+  </h1>
+  <Grid container spacing={2}>
+<Grid item lg={12} md={12} sm={12} xs={12}>
+  <Tabs/>
+  <Card
+  />
+</Grid>
+</Grid>
+  </Container>)
+    }
 }
 
 export default Events;
